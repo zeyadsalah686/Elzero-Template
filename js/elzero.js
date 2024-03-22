0a86965c1caaaccbdb2856775a38a27f08e9fe15
zeyadsalah686 <137503900+zeyadsalah686@users.noreply.github.com>
2024-03-22 23:11:31 +0200

    Update elzero.js

--- js/elzero.js
+++ js/elzero.js
@@ -33,28 +33,28 @@ const updateCountDown = setInterval(() => {
 
 // Count From Zero To Number In Stats Section
 
-let nums = document.querySelectorAll(".stats .number");
-let statsSection = document.querySelector(".stats");
-let started = false;
+// let nums = document.querySelectorAll(".stats .number");
+// let statsSection = document.querySelector(".stats");
+// let started = false;
 
-window.onscroll = () => {
-    if (window.scrollY >= statsSection.offsetTop - 520) {
-        if (!started) {
-            nums.forEach((num) => startCount(num));
-        }
-        started = true;
-    }
-}
+// window.onscroll = () => {
+//     if (window.scrollY >= statsSection.offsetTop - 520) {
+//         if (!started) {
+//             nums.forEach((num) => startCount(num));
+//         }
+//         started = true;
+//     }
+// }
 
-function startCount(el) {
-    let goal = el.dataset.goal;
-    let count = setInterval(() => {
-        el.textContent++;
-        if (el.textContent == goal) {
-            clearInterval(count);
-        }
-    }, 2000 / goal);
-}
+// function startCount(el) {
+//     let goal = el.dataset.goal;
+//     let count = setInterval(() => {
+//         el.textContent++;
+//         if (el.textContent == goal) {
+//             clearInterval(count);
+//         }
+//     }, 2000 / goal);
+// }
 
 // Scroll To Top
 
